eb9ba42a63f405621594fdb069f6b8e5993bf33b
janet <janet.han@gmail.com>
2020-08-31 19:35:34 -0400

    folder setup

---
+++ main_app/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+
+# Create your views here.
+# Add the following import
+from django.http import HttpResponse
+from .models import Finch
+
+# Define the home view
+
+
+def home(request):
+    return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def finches_index(request):
+    finches = Finch.objects.all()
+    return render(request, 'finches/index.html', {'finches': finches})
+
+
+def finches_detail(request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    return render(request, 'finches/detail.html', {'finch': finch})
